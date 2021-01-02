0c752800b4acdb4b605c75bfeaf41c222303fb29
noob513 <76770812+noob513@users.noreply.github.com>
2021-01-02 11:08:15 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -74,7 +74,7 @@ def get_readable_time(seconds: int) -> str:
 
 PM_START_TEXT = """
 Hi {}, my name is {}! 
-I am an Anime themed group management bot.
+I am an Lodu bhai 😂😂😂😂 Nikl Randi ab  themed group management bot.
 Build by weebs for weebs, I specialize in managing anime and similar themed groups.
 You can find my list of available commands with /help.
 """
