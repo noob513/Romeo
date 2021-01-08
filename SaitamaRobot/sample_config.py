6c2fcc74194715c11d9b1649d2862ce7e5b59614
noob513 <76770812+noob513@users.noreply.github.com>
2021-01-08 13:25:44 +0530

    Update sample_config.py

--- SaitamaRobot/sample_config.py
+++ SaitamaRobot/sample_config.py
@@ -19,8 +19,8 @@ class Config(object):
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 1078841825  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Warning_MadBoy_is_Here"
-    SUPPORT_CHAT = 'Romeo1Bot_Support'  #Your own group for support, do not add the @
+    OWNER_USERNAME = "Swami_2_0_0_5"
+    SUPPORT_CHAT = 'Marvelbotsupport'  #Your own group for support, do not add the @
     JOIN_LOGGER = -1001272082336  #Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = -1001355817633  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
